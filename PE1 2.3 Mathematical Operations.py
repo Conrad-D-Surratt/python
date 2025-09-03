955b6ce069bf10e0447dd973ac512c1b85750b75
Conrad Surratt <csurratt376@students.mchenry.edu>
2025-09-02 22:41:01 -0500

    Rectangle Assignment

---
+++ PE1 2.3 Mathematical Operations.py
@@ -0,0 +1,14 @@
+"""
+Write a Python program to calculate the area of a rectangle:
+Declare two variables, length and width, and assign values to them.
+Calculate the area using the formula: area = length * width.
+Print a message displaying the dimensions and the area using concatenation or formatting.
+"""
+
+length = 15
+width = 10
+area = length * width
+print ("The area of a the rectangle with a length of" , length , "and a width of" , width , "is" , area)
+#I could've simply used print to state at least the length and width, but it was more fun this way.
+#I couldn't add a period at the end unfortunately, at least not without using a string concentration, but we haven't learned about those in the modules yet.
+#I know about the string concentrations already because I already finished the entire 2nd module on the Python Institute before starting this assignment.
