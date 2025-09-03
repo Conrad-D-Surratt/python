511f8e8cd9cda34d427302a7f25d8b427b8e23dc
Conrad Surratt <csurratt376@students.mchenry.edu>
2025-09-02 22:25:35 -0500

    Data Types Assignment

---
+++ Understanding Data Types in Python.py
@@ -0,0 +1,44 @@
+"""
+Copy the sample program into your Python editor. Run the program and observe the output. Change the variable values, test the program, and submit your file. 
+Sample program: # This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 10)
+
+# Integer
+y = -20
+print("Integer:", y)
+
+# Float
+z = 15.5
+print("Float:", z)
+
+# String
+name = "John"
+print("String:", name)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
+"""
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 25)
+
+# Integer
+y = 300000000
+print("Integer:", y)
+
+# Float
+z = 0.00000000001
+print("Float:", z)
+
+# String
+name = "Batman"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
