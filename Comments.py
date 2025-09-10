c3725dc09b0254ccf9bb9777386efb9a53f19bda
Conrad Surratt <csurratt376@students.mchenry.edu>
2025-09-09 22:41:26 -0500

    Comments Assignment

---
+++ Comments.py
@@ -0,0 +1,19 @@
+"""tank_size = 15
+# Define the total distance covered
+distance = 600
+# Calculate miles per gallon
+mpg = distance / tank_size
+# Print the result
+print("Miles per gallon:", mpg)
+Type the above code into Visual Studio Code.
+Add a minimum of 4 comments to the above Python program to make it more understandable."""
+
+"""Calculate and display miles per gallon using the fuel tank size and distance"""
+# TODO: Make the tank_size and distance user inputs in order to make it into a true calculator
+tank_size = 15 #size of the tank in gallons
+# Define the total distance covered
+distance = 600 #distance in miles
+# Calculate miles per gallon
+mpg = distance / tank_size
+# Print the result
+print("Miles per gallon:", mpg)
